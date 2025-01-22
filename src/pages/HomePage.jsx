8f82e6763175b9d215c422141dccb7830e2932b8
lloydllim <37279932+lloydllim@users.noreply.github.com>
2025-01-22 21:18:50 +0800

    update homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,16 @@
+import HeroComponent from '@/components/HeroComponent';
+import SecondaryNavbar from '@/components/navbar/SecondaryNavbar';
+import OngoingBidCard from '@/components/OngoingBidCard';
 import ProductCard from '@/components/ProductCard';
 
 const HomePage = () => {
   return (
     <>
+      <HeroComponent />
+      <SecondaryNavbar />
       <div className='flex flex-row mt-5 space-x-4'>
         <ProductCard />
+        <OngoingBidCard />
       </div>
     </>
   );
